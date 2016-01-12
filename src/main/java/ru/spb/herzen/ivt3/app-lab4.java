4026f359267a92164e1765f13b8420ad0a4d8e37
knastin <knastin@users.noreply.github.com>
2016-01-12 20:57:48 +0300

    Update app-lab4.java

--- src/main/java/ru/spb/herzen/ivt3/app-lab4.java
+++ src/main/java/ru/spb/herzen/ivt3/app-lab4.java
@@ -1,1 +1,9 @@
+package ivt3.herzen.spb.ru;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class string {
+    public static void main(String[] args) {
